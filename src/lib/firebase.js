eded2050ad60557cf91ace6d2d63037c0a2f05f2
Subham Chowdhur <sc761851@gmail.com>
2021-12-15 02:14:01 +0530

    firebase connected and database is seeded

---
+++ src/lib/firebase.js
@@ -0,0 +1,25 @@
+import Firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore';
+import 'firebase/compat/firestore';
+
+//import { seedDatabase } from '../seed';
+const config = {
+    apiKey: "AIzaSyDMNfCYjvJLJnUvW-uSFbqME5d9uDBWcGM",
+
+    authDomain: "instagram-d60b9.firebaseapp.com",
+
+    projectId: "instagram-d60b9",
+
+    storageBucket: "instagram-d60b9.appspot.com",
+
+    messagingSenderId: "41130961488",
+
+    appId: "1:41130961488:web:59b31dbaa18c9c17177f55"
+
+};
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = Firebase.firestore
+
+//seedDatabase(firebase);
+
+export { firebase, FieldValue };
